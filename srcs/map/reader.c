6cf4d15448065d6da2814124c9f1dd7a327b4927
Lucas LELIEVRE <llelievr@student.42.fr>
2019-03-01 17:12:31 +0100

    fix reader norm

--- srcs/map/reader.c
+++ srcs/map/reader.c
@@ -6,7 +6,7 @@
 /*   By: llelievr <llelievr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/17 22:46:01 by llelievr          #+#    #+#             */
-/*   Updated: 2019/02/24 22:35:08 by llelievr         ###   ########.fr       */
+/*   Updated: 2019/03/01 17:10:29 by llelievr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 #include <unistd.h>
 #define BUFF_SIZE 1
 
-static char	*get_file_content(char *file, int *content_len)
+static char			*get_file_content(char *file, int *content_len)
 {
 	int		fd;
 	char	*content;
-	char 	buf[BUFF_SIZE + 1];
+	char	buf[BUFF_SIZE + 1];
 	int		rd;
 
 	if ((fd = open(file, O_RDONLY)) == -1)
@@ -43,60 +43,56 @@ static char	*get_file_content(char *file, int *content_len)
 static t_texture	**load_textures(t_world *world, t_json_object *obj)
 {
 	t_json_array	*arr;
-	t_json_element	*elem;
-	t_texture		**textures;
+	t_json_element	*e;
+	t_texture		**tex;
 	int				i;
-	
-	if(!(arr = json_get_array(obj, "textures")))
+
+	if (!(arr = json_get_array(obj, "textures")) ||
+	!(tex = (t_texture **)malloc(sizeof(t_texture *) * (arr->elems_count))))
 		return (NULL);
-	if (!(textures = (t_texture **)malloc(sizeof(t_texture *) * (arr->elems_count))))
-		return (NULL);
-	elem = arr->elements;
+	e = arr->elements;
 	i = 0;
-	while (elem)
+	while (e)
 	{
-		if (elem->value->type != JSON_OBJECT 
-			|| !(textures[i++] = load_json_texture((t_json_object *)elem->value)))
+		if (e->value->type != JSON_OBJECT
+			|| !(tex[i++] = load_json_texture((t_json_object *)e->value)))
 		{
-			//TODO: clean previous loaded textures
-			ft_putstr("Invalid texture at index: ");
+			ft_putstr("Invalid texture at index: "); //TODO: clean previous loaded textures
 			ft_putnbr(i);
 			ft_putchar('\n');
-			free(textures);
+			free(tex);
 			return (NULL);
 		}
-		elem = elem->next;
+		e = e->next;
 	}
 	world->textures_count = arr->elems_count;
-	return (textures);
+	return (tex);
 }
 
 static t_block		**load_blocks(t_world *world, t_json_object *obj)
 {
 	t_json_array	*arr;
-	t_json_element	*elem;
+	t_json_element	*e;
 	t_block			**blocks;
 	int				i;
-	
-	if(!(arr = json_get_array(obj, "blocks")))
+
+	if (!(arr = json_get_array(obj, "blocks")) ||
+		!(blocks = (t_block **)malloc(sizeof(t_block *) * (arr->elems_count))))
 		return (NULL);
-	if (!(blocks = (t_block **)malloc(sizeof(t_block *) * (arr->elems_count))))
-		return (NULL);
-	elem = arr->elements;
+	e = arr->elements;
 	i = 0;
-	while (elem)
+	while (e)
 	{
-		if (elem->value->type != JSON_OBJECT 
-			|| !(blocks[i++] = load_json_block(world, (t_json_object *)elem->value)))
+		if (e->value->type != JSON_OBJECT
+		|| !(blocks[i++] = load_json_block(world, (t_json_object *)e->value)))
 		{
-			//TODO: clean previous loaded blocks
-			ft_putstr("Invalid block at index: ");
+			ft_putstr("Invalid block at index: ");//TODO: clean previous loaded blocks
 			ft_putnbr(i - 1);
 			ft_putchar('\n');
 			free(blocks);
 			return (NULL);
 		}
-		elem = elem->next;
+		e = e->next;
 	}
 	world->blocks_count = arr->elems_count;
 	return (blocks);
@@ -106,12 +102,12 @@ static t_bool		load_map(t_world *world, t_json_object *obj)
 {
 	double	*texture_i;
 
-	if(!(obj = json_get_object(obj, "map")))
+	if (!(obj = json_get_object(obj, "map")))
 		return (FALSE);
-	if(!ft_json_vec3(json_object_get(obj, "size"), &world->size))
+	if (!ft_json_vec3(json_object_get(obj, "size"), &world->size))
 		return (FALSE);
 	printf("Size %f %f\n", world->size.x, world->size.y);
-	world->data = load_map_data(world,  json_object_get(obj, "data"));
+	world->data = load_map_data(world, json_object_get(obj, "data"));
 	if (!(texture_i = json_get_number(obj, "ceil_texture")))
 		return (FALSE);
 	world->ceil = world->textures[(int)*texture_i];
@@ -121,7 +117,7 @@ static t_bool		load_map(t_world *world, t_json_object *obj)
 	return (TRUE);
 }
 
-void			load_world(t_world *world, char *file)
+void				load_world(t_world *world, char *file)
 {
 	t_json_state	state;
 	t_json_value	*val;
@@ -137,9 +133,9 @@ void			load_world(t_world *world, char *file)
 		return ;
 	}
 	free((void *)content);
-	if(!(world->textures = load_textures(world, (t_json_object *)val)))
+	if (!(world->textures = load_textures(world, (t_json_object *)val)))
 		return ;
-	if(!(world->blocks = load_blocks(world, (t_json_object *)val)))
+	if (!(world->blocks = load_blocks(world, (t_json_object *)val)))
 		return ;
 	load_map(world, (t_json_object *)val);
 	json_free_value(val);
