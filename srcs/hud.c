8a8a5f72c6150b732d3c65c5f5f67434dfff3c87
David DE JESUS DUARTE <dde-jesu@student.42.fr>
2019-03-01 18:09:23 +0100

    Norm minimap

--- srcs/hud.c
+++ srcs/hud.c
@@ -6,7 +6,7 @@
 /*   By: llelievr <llelievr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 23:45:21 by llelievr          #+#    #+#             */
-/*   Updated: 2019/03/01 17:47:52 by dde-jesu         ###   ########.fr       */
+/*   Updated: 2019/03/01 18:04:01 by dde-jesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	render_hud(t_wolf *wolf)
 {
-	const t_texture_animated	*weapons = &wolf->weapons_texture;
+	t_texture_animated *const	weapons = &wolf->weapons_texture;
 
 	apply_texture_blended(wolf->img, weapons,
-		(SDL_Rect){ 0, 0, weapons.super.size.x, weapons.super.size.y },
+		(SDL_Rect){ 0, 0, weapons->super.size.x, weapons->super.size.y },
 		(SDL_Rect){ S_WIDTH_2 - (S_WIDTH / 3) / 2, S_HEIGHT - (S_WIDTH / 3) + 1,
 					(S_WIDTH / 3), (S_WIDTH / 3) });
 	apply_texture_blended(wolf->img, &wolf->heads_texture,
@@ -27,15 +27,16 @@ void	render_hud(t_wolf *wolf)
 	apply_surface_blended(wolf->img, wolf->head_overlay,
 		(SDL_Rect){ 0, 0, wolf->head_overlay->w + 5, wolf->head_overlay->h + 5},
 		(SDL_Rect){ S_WIDTH - 100 - 12, S_HEIGHT - 120 - 12, 110, 130 });
-	if (SDL_GetTicks() - weapons.last_seen > weapons.delay)
+	if (SDL_GetTicks() - weapons->last_seen > weapons->delay)
 	{
 		if (!wolf->player.shooting)
 			return ;
-		weapons.index++;
-		weapons.last_seen = SDL_GetTicks();
-		if ((weapons.index % (int)weapons.step_count.x) == 0)
+		weapons->index++;
+		weapons->last_seen = SDL_GetTicks();
+		if ((weapons->index % (int)weapons->step_count.x) == 0)
 		{
-			weapons.index = wolf->player.selected_weapon * weapons.step_count.x;
+			weapons->index = wolf->player.selected_weapon
+				* weapons->step_count.x;
 			wolf->player.shooting = FALSE;
 		}
 	}
--- srcs/minimap.c
+++ srcs/minimap.c
@@ -6,7 +6,7 @@
 /*   By: llelievr <llelievr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/19 18:33:21 by llelievr          #+#    #+#             */
-/*   Updated: 2019/02/24 23:58:10 by llelievr         ###   ########.fr       */
+/*   Updated: 2019/03/01 18:08:00 by dde-jesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@ static int			compute_block_minimap(t_wolf *wolf, t_vec2 map)
 {
 	t_block_state	*state;
 
+	if (map.x <= wolf->player.pos.x + 0.2
+			&& map.x >= wolf->player.pos.x - 0.2
+			&& map.y <= wolf->player.pos.y + 0.2
+			&& map.y >= wolf->player.pos.y - 0.2)
+		return (0xFF0000);
 	state = wolf->world.data[(int)map.y][(int)map.x];
 	if (!state)
 		return (-1);
@@ -31,36 +36,53 @@ static int			compute_block_minimap(t_wolf *wolf, t_vec2 map)
 
 static void			render_fov(t_wolf *wolf)
 {
-	const t_vec2	s = { (float)wolf->minimap_size / wolf->world.size.x,
-		 (float)wolf->minimap_size / wolf->world.size.y};
-	uint32_t 	i;
-	t_vec2	pos;
+	t_vec2		s;
+	uint32_t	i;
+	t_vec2		pos;
+	float		dist;
 
+	s = (t_vec2) { (float)wolf->minimap_size / wolf->world.size.x,
+		(float)wolf->minimap_size / wolf->world.size.y};
 	i = -1;
 	while (++i < wolf->stats.num_rays)
 	{
 		pos = (t_vec2){
-		wolf->last_rays[i].start.x * s.x,
-		wolf->last_rays[i].start.y * s.y};
-		float dist = wolf->last_rays[i].dist - wolf->last_rays[i].extra_dist;
+			wolf->last_rays[i].start.x * s.x,
+			wolf->last_rays[i].start.y * s.y
+		};
+		dist = wolf->last_rays[i].dist - wolf->last_rays[i].extra_dist;
 		draw_line(wolf->img, (t_pixel){
-		S_WIDTH - wolf->minimap_size - wolf->minimap_padding_x + pos.x,
-		pos.y + wolf->minimap_padding_y, BACKGROUND_COLOR_FOV},
-		(t_pixel){
-			S_WIDTH - wolf->minimap_size - wolf->minimap_padding_x + (pos.x + (wolf->last_rays[i].dir.x * dist * s.x)),
-			pos.y + (wolf->last_rays[i].dir.y * dist * s.y) + wolf->minimap_padding_y , 0});
+				S_WIDTH - wolf->minimap_size - wolf->minimap_padding_x + pos.x,
+				pos.y + wolf->minimap_padding_y, BACKGROUND_COLOR_FOV},
+			(t_pixel){
+				S_WIDTH - wolf->minimap_size - wolf->minimap_padding_x
+				+ (pos.x + (wolf->last_rays[i].dir.x * dist * s.x)),
+				pos.y + (wolf->last_rays[i].dir.y * dist * s.y)
+				+ wolf->minimap_padding_y, 0});
 	}
+}
+
+static void			render_minimap_overlay(t_wolf *wolf)
+{
+	stroke_rect(wolf->img, 0, (SDL_Rect) {
+			S_WIDTH - wolf->minimap_size - wolf->minimap_padding_x,
+			wolf->minimap_padding_y, wolf->minimap_size, wolf->minimap_size });
+	stroke_rect(wolf->img, 0, (SDL_Rect) {
+			S_WIDTH - wolf->minimap_size - wolf->minimap_padding_x + 1,
+			wolf->minimap_padding_y + 1,
+			wolf->minimap_size - 2, wolf->minimap_size - 2 });
 }
 
 void				render_minimap(t_wolf *wolf)
 {
-	uint32_t	i;
-	uint32_t	j;
-	const t_vec2	s = { wolf->world.size.x / (float)wolf->minimap_size,
-		wolf->world.size.y / (float)wolf->minimap_size };
-	t_vec2 map;
-	int color;
+	uint32_t		i;
+	uint32_t		j;
+	t_vec2			s;
+	int				index;
+	int				color;
 
+	s = (t_vec2) { (float)wolf->world.size.x / wolf->minimap_size,
+		(float)wolf->world.size.y / wolf->minimap_size};
 	render_fov(wolf);
 	i = -1;
 	while (++i < wolf->minimap_size)
@@ -68,16 +90,14 @@ void				render_minimap(t_wolf *wolf)
 		j = -1;
 		while (++j < wolf->minimap_size)
 		{
-			map = (t_vec2){j * s.x, i * s.y};
-			int index = ((i + wolf->minimap_padding_y) * (int)S_WIDTH) + j + (int)S_WIDTH - wolf->minimap_size - wolf->minimap_padding_x;
-			if ((color = compute_block_minimap(wolf, map)) == -1)
-				color = wolf->img->pixels[index] == BACKGROUND_COLOR_FOV ? BACKGROUND_COLOR_FOV : BACKGROUND_COLOR;
-			if (map.x <= wolf->player.pos.x + 0.2 && map.x >= wolf->player.pos.x - 0.2
-			&& map.y <= wolf->player.pos.y + 0.2 && map.y >= wolf->player.pos.y - 0.2)
-				color = 0xFF0000;
+			index = ((i + wolf->minimap_padding_y) * (int)S_WIDTH) + j
+				+ (int)S_WIDTH - wolf->minimap_size - wolf->minimap_padding_x;
+			if ((color = compute_block_minimap(wolf,
+							(t_vec2){j * s.x, i * s.y})) == -1)
+				color = wolf->img->pixels[index] == BACKGROUND_COLOR_FOV
+					? BACKGROUND_COLOR_FOV : BACKGROUND_COLOR;
 			wolf->img->pixels[index] = color;
 		}
 	}
-	stroke_rect(wolf->img, 0, (SDL_Rect) { S_WIDTH - wolf->minimap_size - wolf->minimap_padding_x, wolf->minimap_padding_y, wolf->minimap_size, wolf->minimap_size });
-	stroke_rect(wolf->img, 0, (SDL_Rect) { S_WIDTH - wolf->minimap_size - wolf->minimap_padding_x + 1, wolf->minimap_padding_y + 1, wolf->minimap_size - 2, wolf->minimap_size - 2 });
+	render_minimap_overlay(wolf);
 }
