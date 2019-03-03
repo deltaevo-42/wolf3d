940a25f14a7e710fa5767815e655616e4bb76abc
David DE JESUS DUARTE <dde-jesu@student.42.fr>
2019-03-03 16:16:33 +0100

    Norm main

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llelievr <llelievr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dde-jesu <dde-jesu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 17:12:55 by llelievr          #+#    #+#             */
-/*   Updated: 2019/03/02 18:17:38 by llelievr         ###   ########.fr       */
+/*   Updated: 2019/03/03 16:15:18 by dde-jesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,31 +17,23 @@ int		main(int argc, char **argv)
 	t_wolf	wolf;
 
 	wolf = (t_wolf) {
-		.player = {
-			.rotation = 0.01,
-			.selected_weapon = 0,
-		},
+		.player = { .rotation = 0.01, .selected_weapon = 0 },
 		.dist_to_plane = 1.0,
 		.minimap_size = 100,
-		.minimap_padding_x = 10,
-		.minimap_padding_y = 10,
-		.weapons_texture = { 
-			.step_count = { 5, 4 },
-			.step_size = { 64, 64 },
-			.spacer = { 1, 1 },
-			.delay = 50
+		.minimap_padding_x = 10, .minimap_padding_y = 10,
+		.weapons_texture = {
+			.step_count = { 5, 4 }, .step_size = { 64, 64 },
+			.spacer = { 1, 1 }, .delay = 50
 		},
-		.heads_texture = { 
-			.step_count = { 3, 7 },
-			.step_size = { 24, 32 },
-			.spacer = { 1, 1 },
-			.delay = 500
+		.heads_texture = {
+			.step_count = { 3, 7 }, .step_size = { 24, 32 },
+			.spacer = { 1, 1 }, .delay = 500
 		}
 	};
 	if (!load_config(&wolf, "assets/maps/map_1.json"))
 	{
 		ft_putendl("Unable to load world");
-		return (1);//FREE 
+		return (1);//FREE
 	}
 	sdl_init(&wolf);
 	game_loop(&wolf);
