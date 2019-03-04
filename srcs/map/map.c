a5c9f69e7f85684331b31adb2bd464ed74bab4d3
David DE JESUS DUARTE <dde-jesu@student.42.fr>
2019-03-04 16:36:27 +0100

    Norm map/map

--- srcs/map/map.c
+++ srcs/map/map.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   map.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llelievr <llelievr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dde-jesu <dde-jesu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/19 12:29:48 by llelievr          #+#    #+#             */
-/*   Updated: 2019/03/04 13:46:44 by llelievr         ###   ########.fr       */
+/*   Updated: 2019/03/04 16:35:45 by dde-jesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf.h"
 
-t_block_state		*create_block_state(t_wolf *wolf, int block_id)
+static t_block_state	*create_block_state(t_wolf *wolf, int block_id)
 {
 	t_block_state	*state;
 
@@ -36,30 +36,30 @@ t_block_state		*create_block_state(t_wolf *wolf, int block_id)
 	return (state);
 }
 
-t_bool				load_map_cols(t_wolf *wolf, t_world *w, int i,
+static t_bool			load_map_cols(t_wolf *wolf, t_world *w,
 	t_json_element *col_elem, t_block_state ***s)
 {
 	int		j;
 	int		block_id;
 
-	if (!(s[i] = (t_block_state **)malloc(sizeof(t_block_side *) * w->size.x)))
+	if (!(*s = (t_block_state **)malloc(sizeof(t_block_side *) * w->size.x)))
 		return (FALSE);
 	j = 0;
 	while (col_elem)
 	{
 		if (col_elem->value->type != JSON_NUMBER)
-			return (unload_map_col(s[i], j));
+			return (unload_map_col(*s, j));
 		block_id = (int)((t_json_number *)col_elem->value)->value - 1;
 		if (block_id == -1)
-			s[i][j++] = NULL;
-		else if (!(s[i][j++] = create_block_state(wolf, block_id)))
-			return (unload_map_col(s[i], j - 1));
+			(*s)[j++] = NULL;
+		else if (!((*s)[j++] = create_block_state(wolf, block_id)))
+			return (unload_map_col(*s, j - 1));
 		col_elem = col_elem->next;
 	}
 	return (TRUE);
 }
 
-t_block_state		***load_map_data(t_wolf *wolf, t_world *w,
+static t_block_state	***load_map_data(t_wolf *wolf, t_world *w,
 	t_json_value *val)
 {
 	t_json_array	*arr;
@@ -77,17 +77,16 @@ t_block_state		***load_map_data(t_wolf *wolf, t_world *w,
 	{
 		if (row_elem->value->type != JSON_ARRAY ||
 			((t_json_array *)row_elem->value)->elems_count != w->size.x)
-			return ((void *)unload_map(states, i, w->size.x));
+			return ((void *)(uintptr_t)unload_map(states, i, w->size.x));
 		col_elem = ((t_json_array *)row_elem->value)->elements;
-		if (!load_map_cols(wolf, w, i++, col_elem, states))
-			return ((void *)unload_map(states, i - 1, w->size.x));
+		if (!load_map_cols(wolf, w, col_elem, states + i++))
+			return ((void *)(uintptr_t)unload_map(states, i - 1, w->size.x));
 		row_elem = row_elem->next;
 	}
 	return (states);
 }
 
-
-t_bool		load_map(t_wolf *wolf, t_world *w, t_json_object *obj)
+t_bool					load_map(t_wolf *wolf, t_world *w, t_json_object *obj)
 {
 	double	*texture_i;
 
